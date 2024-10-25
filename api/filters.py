ca2f6fae65bfc3969a4887c56c494a195cd3eea3
nursultan12080808 <nursultan.top.game@gmail.com>
2024-10-25 11:32:11 +0600

    commit

--- api/filters.py
+++ api/filters.py
@@ -1,8 +1,9 @@
 import django_filters
-
-from shark_app.models import *
+from shark_app.models import News
 
 class NewsFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')  # Нечувствительный к регистру и частичное совпадение
+
     class Meta:
         model = News
         fields = ["category", "tags", "user", "location", "rating", "name"]
